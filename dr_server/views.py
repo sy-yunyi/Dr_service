2b7051e8cad148a19366e49533428d19385b0dbd
six <1293244060@qq.com>
2020-07-01 17:35:24 +0800

    format delay1

--- dr_server/views.py
+++ dr_server/views.py
@@ -204,7 +204,7 @@ class ConferenceList(APIView):
             confer_dict["shortName"] = conf.con_sname
             confer_dict["property"] = ["截稿日期："+str(conf.con_paper_deadline),"地点："+ str(conf.con_where)]
             dy=conf.con_delay if conf.con_delay!=" " else ""
-            if conf.con_rank1!="" and conf.con_rank1!=" " and conf.con_rank1!="暂无" and conf.con_delay!=" ":
+            if conf.con_rank1!="" and conf.con_rank1!=" " and conf.con_rank1!="暂无" and conf.con_delay!=" " and conf.con_delay!="":
                 confer_dict["rate"] = ["CCF: "+ str(conf.con_rank1),conf.con_delay]
             elif conf.con_rank1!="" and conf.con_rank1!=" " and conf.con_rank1!="暂无":
                 confer_dict["rate"] =["CCF: "+ str(conf.con_rank1)]
